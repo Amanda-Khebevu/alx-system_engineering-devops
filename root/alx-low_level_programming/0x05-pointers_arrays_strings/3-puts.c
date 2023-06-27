32708e04fd034533bd7057efedee77d1bddcd843
Amanda-Khebevu <amandakhebevu@gmail.com>
2023-06-27 13:52:03 -0700

    I do not fear computers

---
+++ root/alx-low_level_programming/0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts - Funtion that print charaters in the String
+*
+*@str: the string
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
