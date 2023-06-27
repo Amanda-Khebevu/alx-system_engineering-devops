7d104fff1624357bce5d0b3afcebb5802d693e6e
Amanda-Khebevu <amandakhebevu@gmail.com>
2023-06-27 13:47:45 -0700

    This report, by its very length

---
+++ root/alx-low_level_programming/0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include  "main.h"
+
+/**
+*_strlen - Funtion that return the lenght of a String
+*
+*@s: String
+*Return: Return the lenght
+*/
+
+int _strlen(char *s)
+{
+	int lenght;
+
+	for (lenght = 0; *s; s++)
+		lenght++;
+
+	return (lenght);
+
+}
