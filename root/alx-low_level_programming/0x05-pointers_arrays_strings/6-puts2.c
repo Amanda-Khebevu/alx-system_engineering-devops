4d6bd4b7f9067948c3ba14a533dcfb883dcc9533
Amanda-Khebevu <amandakhebevu@gmail.com>
2023-06-27 14:01:01 -0700

    Half the lies they tell about me aren't true

---
+++ root/alx-low_level_programming/0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include " main.h"
+
+/**
+* puts2 - prints a string
+*
+* @str: character
+*/
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
