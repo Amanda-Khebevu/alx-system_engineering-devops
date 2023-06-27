bd4bcf619c870479a213818694153cfa79ce4690
Amanda-Khebevu <amandakhebevu@gmail.com>
2023-06-27 13:43:36 -0700

    Don't swap horses in crossing a stream

---
+++ root/alx-low_level_programming/0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*swap_int - Funtion that swap the value into 2 pointers
+*
+*@a: pointer Integer 1
+*@b: pointer Integer 2
+*/
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	*a = *b;
+	*b = i;
+}
